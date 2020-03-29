c3300eb160fe264ea0b5edac6c1c4376f2911d9e
Nemesiss.Lin <353388639@qq.com>
2020-03-29 13:14:59 +0800

    Change TokenExchangeCodeMismatch exception error code

--- src/main/java/com/nemesiss/dev/oauthplayground/Exception/TokenExchangeCodeMismatchException.java
+++ src/main/java/com/nemesiss/dev/oauthplayground/Exception/TokenExchangeCodeMismatchException.java
@@ -2,6 +2,6 @@ package com.nemesiss.dev.oauthplayground.Exception;
 
 public class TokenExchangeCodeMismatchException extends PlaygroundException {
     public TokenExchangeCodeMismatchException(String CurrentCode) {
-        super("The original declared token exchange code is mismatch with: " + CurrentCode + ".", 1005);
+        super("The original declared token exchange code is mismatch with: " + CurrentCode + ".", 1006);
     }
 }
